4d972e371a1639c15b8d226c68dd54f8f451c8c8
Theodlz <theophile.dulaz@gmail.com>
2025-12-03 16:43:48 +0100

    handle ctrl+c for stopping, and put a timeount on message polling

--- Fritz/main.py
+++ Fritz/main.py
@@ -51,92 +51,103 @@ feature_scaler = load(f"{base_dir}/feature_scaler_PAstro.joblib")
 
 while True:
     try:
-        for message in consumer.consume():
-            value = message.value()
-            parsed = parse_gcn(value)
-            params = get_params(parsed)
+        # for message in consumer.consume():
+        # let's have a timeout of 1 second to check for KeyboardInterrupt
+        message = consumer.poll(timeout=1.0)
+        if message is None:
+            continue
+        value = message.value()
+        parsed = parse_gcn(value)
+        params = get_params(parsed)
+        (
+        superevent_id,
+            event_page,
+            alert_type,
+            group,
+            prob_bbh,
+            prob_bns,
+            prob_nsbh,
+            far_format,
+            distmean,
+            area_90,
+            longitude,
+            latitude,
+            has_ns,
+            has_remnant,
+            has_mass_gap,
+            significant,
+            prob_ter,
+            skymap,
+            PAstro,
+            time,
+            _,
+            _,
+            _,
+        ) = params
+
+        if alert_type == "RETRACTION" or distmean == "error":
+            print(f"Skipping {superevent_id} ({alert_type})")
+            continue
+
+        print(f"Processing {superevent_id} ({alert_type})")
+        model = load(f"{base_dir}/LSTMpredLC__PAstro.joblib")
+        X = np.vstack(
             (
-                superevent_id,
-                event_page,
-                alert_type,
-                group,
-                prob_bbh,
-                prob_bns,
-                prob_nsbh,
-                far_format,
-                distmean,
                 area_90,
+                distmean,
                 longitude,
                 latitude,
                 has_ns,
                 has_remnant,
                 has_mass_gap,
-                significant,
-                prob_ter,
-                skymap,
                 PAstro,
-                time,
-                _,
-                _,
-                _,
-            ) = params
+            )
+        ).T
 
-            if alert_type != "RETRACTION" and distmean != "error":
-                print(f"Processing {superevent_id} ({alert_type})")
-                model = load(f"{base_dir}/LSTMpredLC__PAstro.joblib")
-                X = np.vstack(
-                    (
-                        area_90,
-                        distmean,
-                        longitude,
-                        latitude,
-                        has_ns,
-                        has_remnant,
-                        has_mass_gap,
-                        PAstro,
-                    )
-                ).T
+        # Time array
+        t_min = 0.1
+        t_max = 14.0
+        num_time_points = 71
+        dt = 0.2
+        time_single = np.linspace(t_min, t_max, num_time_points)
+        filter_order = 3
 
-                # Time array
-                t_min = 0.1
-                t_max = 14.0
-                num_time_points = 71
-                dt = 0.2
-                time_single = np.linspace(t_min, t_max, num_time_points)
-                filter_order = 3
+        # Standardize the target data
+        X_new = feature_scaler.transform(X)
 
-                # Standardize the target data
-                X_new = feature_scaler.transform(X)
+        # Reshape X data for LSTM input based on the model's input shape
+        X_new_reshaped = X_new.reshape((X_new.shape[0], 1, X_new.shape[1]))
 
-                # Reshape X data for LSTM input based on the model's input shape
-                X_new_reshaped = X_new.reshape((X_new.shape[0], 1, X_new.shape[1]))
+        mean_preds_new, uncertainty_new = predict_with_uncertainty(
+            model, X_new_reshaped, n_iter=1000
+        )
 
-                mean_preds_new, uncertainty_new = predict_with_uncertainty(
-                    model, X_new_reshaped, n_iter=1000
-                )
+        # Reshape the mean predictions to match the shape used during scaling (num_samples, num_time_points * num_filters)
+        mean_preds_flat = mean_preds_new.reshape(
+            mean_preds_new.shape[0], num_time_points * 3
+        )
+        # Invert the standardization for the mean predictions
+        mean_preds_inverted = target_scaler.inverse_transform(
+            mean_preds_flat
+        ).reshape(mean_preds_new.shape[0], num_time_points, 3)
 
-                # Reshape the mean predictions to match the shape used during scaling (num_samples, num_time_points * num_filters)
-                mean_preds_flat = mean_preds_new.reshape(
-                    mean_preds_new.shape[0], num_time_points * 3
-                )
-                # Invert the standardization for the mean predictions
-                mean_preds_inverted = target_scaler.inverse_transform(
-                    mean_preds_flat
-                ).reshape(mean_preds_new.shape[0], num_time_points, 3)
+        # Reshape uncertainty to match mean_preds_inverted shape
+        uncertainty_reshaped = uncertainty_new.reshape(
+            uncertainty_new.shape[0], num_time_points, 3
+        )
 
-                # Reshape uncertainty to match mean_preds_inverted shape
-                uncertainty_reshaped = uncertainty_new.reshape(
-                    uncertainty_new.shape[0], num_time_points, 3
-                )
+        plot_all_light_curves_with_uncertainty(
+            time_single,
+            mean_preds_inverted,
+            uncertainty_reshaped,
+            superevent_id,
+            time,
+        )
 
-                plot_all_light_curves_with_uncertainty(
-                    time_single,
-                    mean_preds_inverted,
-                    uncertainty_reshaped,
-                    superevent_id,
-                    time,
-                )
-
+    # listen to control-C to exit
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
     except Exception as e:
         print(e)
         traceback.print_exc()
